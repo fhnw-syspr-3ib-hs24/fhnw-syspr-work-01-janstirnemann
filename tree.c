4db45ccdc08b513e06ff9580b37048de47bd7a98
janstirnemann <jan.stirnemann@students.fhnw.ch>
2024-10-12 14:04:40 +0200

    my_copy.c

--- tree.c
+++ tree.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
     struct node {
         char label[32];
-        struct node * left;
+        struct node * left; // Man darf auf etwas zeigen, dass noch nicht fertig definiert ist
         struct node * right;
     };
 
@@ -13,13 +15,33 @@ int main(void) {
     struct node ll = {"left left", NULL, NULL};
     struct node l = {"left", &ll, &lr};
     struct node r = {"right", NULL, NULL};
-    struct node n = {"root", &l, &r};
+    struct node n = {"root", &l, &r}; // Stack
     struct node* root = &n;
+
+    // Man kann es auch so definieren
+
+    //struct node n;   // STACK
+    //n.label = "root";
+    //n.left= NULL;
+    //n.right = NULL;
+
+        // In java Node n = new Node();
+    //struct node * n = malloc(sizeof(struct node));   // HEAP , dynamischer Speicher
+        // In java n.label = "root";
+    //n->label = root;
+    //n->left= NULL;
+    //n->right = NULL;
     
+    assert(n.left == &l);
+
     printf("%s\n", n.label);
-    printf("%s\n", l.label);
+    printf("%s\n", (*(n.left)).label);
+    // printf("%s\n", (n.left)->label);
     printf("%s\n", ll.label);
     printf("%s\n", lr.label);
     printf("%s\n", r.label);
+
+
+    // speicher von den Blättern richtig root wieder freigeben mit free()
     return 0;
 }
